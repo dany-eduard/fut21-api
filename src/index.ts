baa07774ca4895094d224698a1bafaaa184b3701
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-26 01:39:12 -0500

    fix: port server

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ app.use(express.json()) // para que los datos que lleguen en formato json se pue
 app.use(express.urlencoded({ extended: false })) // para que datos enviados desde unformulario se puedan leer
 app.use(indexRoutes) // para que las rutas sean leidas
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000')
 })
