c62bacec0d26ad6e5d65893c3e0eb8e44b1f9a57
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-26 01:39:34 -0500

    add: env keys db

--- src/database.ts
+++ src/database.ts
@@ -2,9 +2,9 @@ require('dotenv').config({ path: '../.env' })
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'jugadores_db',
-  password: 'root',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432
 })
