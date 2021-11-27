e93aa11e9433dea19f16d1b69be698c4ae262b1f
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-26 19:20:46 -0500

    update: connection db

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,8 @@
 require('dotenv').config({ path: '../.env' })
 import { Pool } from 'pg'
 
+const connectionString = process.env.CONNECTION_STRING
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432
+  connectionString
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import indexRoutes from './routes'
 import main from './script'
-
 ;(async () => {
   try {
     await main()
@@ -10,6 +9,8 @@ import main from './script'
   }
 })()
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 // midelwares
@@ -17,6 +18,6 @@ app.use(express.json()) // para que los datos que lleguen en formato json se pue
 app.use(express.urlencoded({ extended: false })) // para que datos enviados desde unformulario se puedan leer
 app.use(indexRoutes) // para que las rutas sean leidas
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
